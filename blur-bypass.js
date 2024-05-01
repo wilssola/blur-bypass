a84f0cd1c3e5f32dfc8d5226027fdcae9561430b
Wilson Oliveira Lima <oliveiralima@protonmail.com>
2024-05-01 19:28:50 -0300

    Update blur-bypass.js

--- blur-bypass.js
+++ blur-bypass.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         Blur Bypass - Passei Direto - Responde Aí
-// @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  try to take over the world!
-// @author       You
+// @namespace    https://github.com/wilssola/blur-bypass
+// @version      0.2
+// @description  Blur Bypass - Passei Direto - Responde Aí
+// @author       Wilson Oliveira Lima
 // @match        *://*.passeidireto.com/*
 // @match        *://*.respondeai.com.br/*
 // @grant        none
@@ -12,21 +12,39 @@
 (function() {
     'use strict';
 
+    setTimeout(blurBypass, 1000);
+})();
+
+function blurBypass() {
     let body = document.getElementsByTagName('body')[0];
-    body.innerHTML = body.innerHTML.replaceAll('blur', '');
+    if (body) {
+        body.innerHTML = body.innerHTML.replaceAll('blur', '');
+    }
 
     let style = document.createElement('style');
-    style.innerHTML = '*, div, div *, .disable-blur, .disable-blur * { filter: blur(0) !important; -webkit-filter: blur(0) !important; }';
+    if (style) {
+        style.innerHTML = '*, div, div *, .disable-blur, .disable-blur * { filter: blur(0) !important; -webkit-filter: blur(0) !important; }';
+    }
 
     let head = document.getElementsByTagName('head')[0];
-    head.appendChild(style);
+    if (head) {
+        head.appendChild(style);
+    }
+
+    let paywall = document.querySelectorAll('[data-testid="free-trial-paywall"]')[0];
+    if (paywall) {
+        paywall.remove();
+    }
 
     let divs = document.getElementsByTagName('div');
-    for (let i in divs) {
-        divs[i].classList.add('disable-blur');
+    for (let div of divs) {
+        if (!div) continue;
+        if (!div.classList) continue;
 
-        if (divs[i].className.includes('banner')) {
-            divs[i].remove();
+        div.classList.add('disable-blur');
+
+        if (div.className.includes('banner')) {
+            div.remove();
         }
     }
-})();
+}
